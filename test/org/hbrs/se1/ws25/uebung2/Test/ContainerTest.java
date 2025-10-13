49334dbe871eaac25b99d6c5b4f89369ee9f7c42
Kfadil2s <khouloud.fadili@smail.inf.h-brs.de>
2025-10-13 21:35:32 +0200

    Mein Code

---
+++ test/org/hbrs/se1/ws25/uebung2/Test/ContainerTest.java
@@ -0,0 +1,56 @@
+package org.hbrs.se1.ws25.uebung2.Test;
+
+import org.hbrs.se1.ws25.uebung2.ActualMember;
+import org.hbrs.se1.ws25.uebung2.Container;
+import org.hbrs.se1.ws25.uebung2.ContainerException;
+import org.hbrs.se1.ws25.uebung2.Member;
+import org.junit.jupiter.api.BeforeEach;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerTest {
+
+    Container co = new Container();
+    Member a = new ActualMember(10);
+    Member b = new ActualMember(20);
+    Member c = new ActualMember(30);
+    Member d = new ActualMember(10);
+
+
+    @org.junit.jupiter.api.Test
+    @BeforeEach
+    void addMember() {
+        try {
+            co.addMember(a);
+            co.addMember(b);
+            co.addMember(c);
+            co.addMember(d);
+            co.addMember(null);
+        } catch (ContainerException e){
+            assertEquals("Das Member-Objekt mit der ID 10 ist bereits vorhanden!",e.getMessage());
+        }
+        assertEquals(3,co.size());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteMember() {
+
+        assertEquals("Das Member-Objekt mit der ID 10 wurde gelöscht", co.deleteMember(10));
+        assertEquals("Das Member-Objekt mit der ID 50 ist nicht verfuegbar", co.deleteMember(50));
+        assertEquals(2,co.size());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void dump() {
+        co.dump();
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+        assertEquals(3,co.size());
+    }
+}
