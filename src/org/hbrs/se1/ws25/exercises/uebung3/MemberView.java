20be95392f3830a2b4fe45aeacf8c14b45db373c
Kfadil2s <khouloud.fadili@smail.inf.h-brs.de>
2025-10-21 22:10:09 +0200

    Mein Code

---
+++ src/org/hbrs/se1/ws25/exercises/uebung3/MemberView.java
@@ -0,0 +1,30 @@
+package org.hbrs.se1.ws25.exercises.uebung3;
+
+import java.util.List;
+
+public class MemberView {
+
+    private static Container c = Container.getInstance();
+
+
+
+
+    public static void view(){
+        List<Member> m = c.getCurrentList();
+        for (int i = 0; i < c.size(); i++) {
+            System.out.println(m.get(i));
+        }
+
+
+    }
+
+    public static void main (String[] args) throws ContainerException {
+
+
+
+        MemberView.view();
+
+    }
+
+
+}
